3dacfe0da694437d1ae26b2faf4e8a19cd620548
MetaYean <meta.y@tecmovecorp.com>
2021-07-20 18:05:00 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [test, setTest] = useState([])
+
+  useEffect(()=> {
+
+    var config = {
+      method: 'get',
+      url: 'http://localhost:3000/test',
+      headers: { }
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(response.data)
+      setTest(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {test.message}
         </p>
         <a
           className="App-link"
